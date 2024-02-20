89b8de49a2bcb7f9cb59d30fc518352ef50d05a6
Jhallam31 <hallam8492@gmail.com>
2024-02-19 20:58:45 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data(){
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    watch: {
+        
+    }
+});
+
+app.mount('#game');
